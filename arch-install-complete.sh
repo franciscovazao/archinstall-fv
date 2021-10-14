82a1b0dea1d4722b743fba8ebc3f96875ff8c536
franciscovazao <74301064+franciscovazao@users.noreply.github.com>
2021-10-15 00:22:53 +0100

    Add files via upload

--- arch-install-complete.sh
+++ arch-install-complete.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo pacman -S apparmor audit bitwarden caprine flatpak firefox hunspell-en_gb hunspell-en_pt papirus-icon-theme powertop power-profiles-daemon reflector seahorse-nautilus signal-desktop thermald torbrowser-launcher ufw vim virt-manager zsh base-devel git intel-ucode linux-firmware -y
+sudo pacman -S apparmor audit bitwarden caprine flatpak firefox hunspell-en_gb papirus-icon-theme powertop power-profiles-daemon reflector seahorse-nautilus signal-desktop thermald torbrowser-launcher ufw vim virt-manager zsh base-devel git intel-ucode linux-firmware libreoffice-fresh -y
 
 cd /opt
 git clone https://aur.archlinux.org/paru.git
@@ -8,7 +8,7 @@ cd paru
 makepkg -si
 cd
 
-paru -S gdm-plymouth gnome-shell-extension-arch-update husnpell-pt_pt intel-hybrid-codec-driver libdgm-plymouth libva-intel-driver-hybrid network-manager-wireguard-git plymouth stacer timeshift -y
+paru -S gdm-plymouth gnome-shell-extension-arch-update hunspell-pt_pt intel-hybrid-codec-driver libgdm-plymouth libva-intel-driver-hybrid networkmanager-wireguard-git plymouth stacer timeshift -y
 
 sudo systemctl enable --now fstrim.timer
 sudo systemctl enable --now ufw.service
@@ -16,3 +16,4 @@ sudo systemctl enable --now apparmor.service
 sudo systemctl enable --now auditd.service
 
 
+flatpak install com.belmoussaoui.Authenticator com.belmoussaoui.Decoder com.dropbox.Clientcom.github.alexhuntley.Plots com.github.gi_lom.dialect com.github.hugolabe.Wike com.github.iwalton3.jellyfin-media-player com.github.johnfactotum.QuickLookup com.github.maoschanz.drawing com.github.tchx84.Flatseal com.spotify.Client com.vscodium.codium com.vscodium.codium org.qbittorrent.qBittorrent us.zoom.Zoom
